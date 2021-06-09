911fa9acbe10be27a8c3ca1ea5f86bdc05273667
Josue AGBOTON <agbotonjosue3@gmail.com>
2021-06-09 11:34:59 +0100

    resolde pb

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies;
+    protected $proxies= '*'; //si on avait l'addresse de notre serveur
 
     /**
      * The headers that should be used to detect proxies.
